77886883cae81dfbf14ab4a6bf19e2e97ed3f517
sjtg <chipilej@gmail.com>
2017-11-21 07:38:10 +0200

    Modified views

--- blog_boards/views.py
+++ blog_boards/views.py
@@ -12,11 +12,4 @@ from .models import Blog_Boards
 #home view page
 def home(request):
 	boards = Blog_Boards.objects.all()
-	blog_board_names = list()
-
-	for board in boards:
-		blog_boards_names.append(board.name)
-
-	response_html = '<br>'.join(blog_board_names)
-
-	return HttpResponse(response_html)
+    	return render(request, 'home.html', {'boards': boards})
