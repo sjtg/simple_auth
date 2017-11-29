d58686d3934b5e427f332bea6c924306587ada47
sjtg <chipilej@gmail.com>
2017-11-29 08:27:24 +0200

    Added email login in the signup forms

--- accounts/views.py
+++ accounts/views.py
@@ -3,20 +3,20 @@ from __future__ import unicode_literals
 
 from django.contrib.auth import login as auth_login
 
-from django.contrib.auth.forms import UserCreationForm
-
 from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 # Create your views here.
 
 #signup function allows the users to create an account
 def signup(request):
 	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
+		form = SignUpForm(request.POST)
 		if form.is_valid():
 			user = form.save()
 			auth_login(request, user)
 			return redirect('home')
 	else:
-		form = UserCreationForm()
+		form = SignUpForm()
 	return render(request, 'signup.html', {'form' : form})
