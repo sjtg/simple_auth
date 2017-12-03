1d41488f7a3a705e7e5372a789d0b8b0c60d11ce
sjtg <chipilej@gmail.com>
2017-12-03 08:39:59 +0200

    Added password_reset files Able to reset users password

--- auth_app/settings.py
+++ auth_app/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
-
+LOGIN_URL = 'login'
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
