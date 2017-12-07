3fd7fb749a3d904358a1ee3f56dcad32a6005ac4
sjtg <chipilej@gmail.com>
2017-12-08 00:52:09 +0200

    Added a link to the search button

--- blog_boards/filters.py
+++ blog_boards/filters.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.models import User
 import django_filters
 
+#from .models import  Blog_Boards
 
 class UserFilter(django_filters.FilterSet):
 	class Meta:
 		model = User
-		fields = ['username',  ]
+		fields = ['username',   ]
